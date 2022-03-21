d7d480708f94f1c333ce0f40b9d9a94701240aba
bikinavisho <bikinavisho@yahoo.com>
2022-03-21 15:09:48 -0500

    Update to 1.5

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,6 +1,5 @@
 ﻿class ModConfig
 {
-	public int RaincoatClothingIndex { get; set; } = 112;
 	public bool RainHoodEnabled { get; set; } = true;
 	public bool RainBootsEnabled { get; set; } = true;
 	public bool RainCoatEnabled { get; set; } = true;
